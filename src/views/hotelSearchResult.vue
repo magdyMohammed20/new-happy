5ab1f14b5d648eed46d57a71e02cd45e65e0d47e
magdyMohammed20 <magdymohammed37@yahoo.com>
2025-12-05 13:57:28 +0200

    1

--- src/views/hotelSearchResult.vue
+++ src/views/hotelSearchResult.vue
@@ -5,7 +5,14 @@
     </el-row>
 </template>
 <script>
-
+import sideAndTopBarSearch from '../components/hotelSearchResult/sideAndTopBarSearch.vue';
+import searchResultCards from '../components/hotelSearchResult/searchResultCards.vue';
+export default {
+    components: {
+        sideAndTopBarSearch,
+        searchResultCards
+    }
+}
 </script>
 <style >
 
